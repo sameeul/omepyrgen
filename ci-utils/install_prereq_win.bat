a1690c2dae749a90baef26f0c989544a5d8b8845
sameeul <sameeul@gmail.com>
2023-07-10 11:57:16 -0400

    update pushd command

--- ci-utils/install_prereq_win.bat
+++ ci-utils/install_prereq_win.bat
@@ -3,7 +3,7 @@ mkdir local_install\include
 
 curl -L https://github.com/sameeul/filepattern/archive/refs/heads/try_cpp_api.zip -o try_cpp_api.zip
 tar -xvf try_cpp_api.zip
-pushd filepattern-try_cpp_api/
+pushd filepattern-try_cpp_api
 mkdir build
 pushd build
 cmake -Dfilepattern_SHARED_LIB=ON -DCMAKE_PREFIX_PATH=../../local_install -DCMAKE_INSTALL_PREFIX=../../local_install ..
