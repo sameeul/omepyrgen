fdf6ce50c2f4cbfbb8bb8f2ca2ab17cdcb984259
sameeul <sameeul@gmail.com>
2023-07-10 17:33:48 -0400

    fixing segfault

--- tests/python/test.py
+++ tests/python/test.py
@@ -13,16 +13,17 @@ def test_single_image():
     print(f"elapsed time = {end_time - start_time}")
 
 def test_image_collection():
-    input_dir = "/home/samee/axle/data/intensity1"
-    stitch_vector_file = "/home/samee/axle/data/tmp_dir/img-global-positions-1.txt";
-    output_dir = "/home/samee/axle/data/test_assembly_out"
+    input_dir = "/mnt/hdd8/axle/data/aaron_test_data"
+    file_pattern = "x{x:d}_y{y:d}_c{c:d}.ome.tiff"
+    print(file_pattern)
+    output_dir = "/mnt/hdd8/axle/data/aaron_test_data_out"
     image_name = "test_image"
     start_time = time()
     pyr_gen = PyramidGenerartor()
-    pyr_gen.generate_from_image_collection(input_dir, stitch_vector_file, image_name, output_dir, 1024, "Viv", "mode_min")
+    pyr_gen.generate_from_image_collection(input_dir, file_pattern, image_name, output_dir, 1024, "Viv", "mean")
     end_time = time()
     print(f"elapsed time = {end_time - start_time}")
 
 if __name__=="__main__":
-    test_single_image()
-    #test_image_collection()
+    #test_single_image()
+    test_image_collection()
